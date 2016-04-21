3fa88f2f8e28f28f017623295b47c3bef6a89601
Josh Houck <Josh123090@gmail.com>
2016-04-21 16:40:29 -0700

    add yelp API

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -6,7 +6,11 @@ var router = express.Router();
 var Restaurant = require('../models/Restaurant.js');
 
 router.get('/', function(req, res) {
+	console.log('gots');
 	Restaurant.getResults(function(err, results) {
-		console.log('results', results);
+		console.log('error: ', err);
+		console.log('results: ', results);
 	});
 });
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 'use strict';
 
+const PORT = 3000;
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 require('dotenv').config();
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/seefood');
-var db = mongoose.conntection;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function() {
     console.log('successfully connected to db');
@@ -19,8 +22,8 @@ db.once('open', function() {
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,15 +31,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/restaurants', require('./routes/restaurants'));
 
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
-});
+// app.all('/*', function(req, res, next) {
+//     // Just send the index.html for other files to support HTML5Mode
+//     res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+// });
 
 
 // catch 404 and forward to error handler
@@ -70,4 +74,10 @@ app.use(function(err, req, res, next) {
   });
 });
 
-module.exports = app;
+// module.exports = app;
+
+var server = http.createServer(app);
+
+server.listen(PORT, function() {
+  console.log(`Server listening on port ${PORT}`);
+});
