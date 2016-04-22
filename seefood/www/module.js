46e447716d73ab51890c7c74ba76ba230b4fb435
Jovvi Johnson <jjohnsow@gmail.com>
2016-04-21 21:39:37 -0700

    cordova

--- seefood/www/module.js
+++ seefood/www/module.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ui.router'])
+angular.module('starter', ['ionic', 'ui.router', 'ngCordova'])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
@@ -45,5 +45,11 @@ angular.module('starter', ['ionic', 'ui.router'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
   });
 })
+
+});
