360ed8da4a548e4357e3d3bca25aa9c1bb5486db
Josh Houck <Josh123090@gmail.com>
2016-04-25 17:22:00 -0700

    removed swipeservice

--- seefood/www/swipe/services/swipeService.js
+++ seefood/www/swipe/services/swipeService.js
@@ -2,24 +2,6 @@
 
 var app = angular.module('seeFoodApp');
 
-app.service('SwipeService', function($http, API, RestaurantService) {
+app.service('SwipeService', function() {
 
-  this.coordObj = {
-    count: 0
-  };
-
-	this.getRestaurants = function(lat, lng) {
-    if(lat || lng) {
-      this.coordObj = {
-      	lat: lat,
-      	lng: lng
-      }
-    }
-    return $http.put(`${API}/restaurants`, this.coordObj)
-    .then(function(res) {
-      this.coordObj.count += res.data.business.length;
-    	RestaurantService.setRestaurants(res.data);
-    	return res;
-    }, err => console.error(err));
-  }
 });
