3eb18b7134c71d9b46df19ca7081421098f610de
Apple <tiffine.koch@gmail.com>
2016-04-27 17:46:06 -0700

    changes of things

--- seefood/www/home/controllers/homeCtrl.js
+++ seefood/www/home/controllers/homeCtrl.js
@@ -5,44 +5,79 @@ var app = angular.module('seeFoodApp');
 app.controller('homeCtrl', function($scope, HomeService, Auth) {
 	console.log('homeCtrl');
 
-	$scope.fbLogin = function() {
-		HomeService.fbLogin();
-	}
-
-	$scope.login = function() {
-		 console.log('fb button clicked');
-
-		 var ref = new Firebase("https://seefoodapp.firebaseio.com");
-			ref.authWithOAuthPopup("facebook", function(error, authData) {
-			  if (error) {
-			    console.log("Login Failed!", error);
-			  } else {
-			    console.log("Authenticated successfully with payload:", authData);
-			  }
-			});
-			// Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
-			// 	// User successfully logged in
-			// }).catch(function(error) {
-			// 	if (error.code === "TRANSPORT_UNAVAILABLE") {
-			// 		Auth.$authWithOAuthPopup("facebook").then(function(authData) {
-			// 			// User successfully logged in. We can log to the console
-			// 			// since we’re using a popup here
-			// 			console.log(authData);
-			// 		});
-			// 	} else {
-			// 		// Another error occurred
-			// 		console.log(error);
-			// 	}
-			// });
-	};
+	$scope.login = function(authMethod) {
+    Auth.$authWithOAuthRedirect(authMethod).then(function(authData) {
+    }).catch(function(error) {
+      if (error.code === 'TRANSPORT_UNAVAILABLE') {
+        Auth.$authWithOAuthPopup(authMethod).then(function(authData) {
+        });
+      } else {
+        console.log(error);
+      }
+    });
+  };
 
 	Auth.$onAuth(function(authData) {
-	 if (authData === null) {
-		 console.log("Not logged in yet");
-	 } else {
-		 console.log("Logged in as", authData.uid);
-	 }
-	 $scope.authData = authData; // This will display the user's name in our view
- });
+		if (authData === null) {
+			console.log('Not logged in yet');
+			//ngMessage, alert user to sign in with facebook
+		} else {
+			console.log('Logged in as', authData.uid);
+			//change state to swipe
+		}
+		// This will display the user's name in our view
+		$scope.authData = authData;
+	});
 
 })
+
+
+
+// 'use strict';
+//
+// var app = angular.module('seeFoodApp');
+//
+// app.controller('homeCtrl', function($scope, HomeService, Auth) {
+// 	console.log('homeCtrl');
+//
+// 	$scope.fbLogin = function() {
+// 		HomeService.fbLogin();
+// 	}
+//
+// 	$scope.login = function() {
+// 		 console.log('fb button clicked');
+//
+// 		 var ref = new Firebase("https://seefoodapp.firebaseio.com");
+// 			ref.authWithOAuthPopup("facebook", function(error, authData) {
+// 			  if (error) {
+// 			    console.log("Login Failed!", error);
+// 			  } else {
+// 			    console.log("Authenticated successfully with payload:", authData);
+// 			  }
+// 			});
+// 			// Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
+// 			// 	// User successfully logged in
+// 			// }).catch(function(error) {
+// 			// 	if (error.code === "TRANSPORT_UNAVAILABLE") {
+// 			// 		Auth.$authWithOAuthPopup("facebook").then(function(authData) {
+// 			// 			// User successfully logged in. We can log to the console
+// 			// 			// since we’re using a popup here
+// 			// 			console.log(authData);
+// 			// 		});
+// 			// 	} else {
+// 			// 		// Another error occurred
+// 			// 		console.log(error);
+// 			// 	}
+// 			// });
+// 	};
+//
+// 	Auth.$onAuth(function(authData) {
+// 	 if (authData === null) {
+// 		 console.log("Not logged in yet");
+// 	 } else {
+// 		 console.log("Logged in as", authData.uid);
+// 	 }
+// 	 $scope.authData = authData; // This will display the user's name in our view
+//  });
+//
+// })
--- seefood/www/home/services/homeService.js
+++ seefood/www/home/services/homeService.js
@@ -13,3 +13,9 @@ app.service('HomeService', function($http, API, CacheFactory) {
  };
 
 })
+
+app.factory('Auth', function($firebaseAuth) {
+    var endPoint = "https://seefoodapp.firebaseio.com";
+    var usersRef = new Firebase(endPoint);
+    return $firebaseAuth(usersRef);
+  })
