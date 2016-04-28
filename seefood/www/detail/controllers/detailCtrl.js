51e8b316db3c964cc6c1a856922ea27c78a375fd
Jovvi Johnson <jjohnsow@gmail.com>
2016-04-28 14:01:22 -0700

    reverted maps

--- seefood/www/detail/controllers/detailCtrl.js
+++ seefood/www/detail/controllers/detailCtrl.js
@@ -22,23 +22,23 @@ app.controller('detailCtrl', function($scope, $stateParams, RestaurantService) {
       labelClass: "marker-labels"
     };
 
-		var homeLatLng = new google.maps.LatLng($scope.restaurant.location.coordinate.latitude, $scope.restaurant.location.coordinate.longitude);
-
-		var marker1 = new MarkerWithLabel({
-       position: homeLatLng,
-       draggable: true,
-       raiseOnDrag: true,
-       map: map,
-       labelContent: "$425K",
-       labelAnchor: new google.maps.Point(22, 0),
-       labelClass: "labels", // the CSS class for the label
-       labelStyle: {opacity: 0.75}
-     });
-
-
-		var iw1 = new google.maps.InfoWindow({
-       content: $scope.restaurant.location.display_address[0] + "\n" + $scope.restaurant.location.display_address[1]
-     });
-     google.maps.event.addListener(marker1, "click", function (e) { iw1.open($scope.map, this); });
+		// var homeLatLng = new google.maps.LatLng($scope.restaurant.location.coordinate.latitude, $scope.restaurant.location.coordinate.longitude);
+		//
+		// var marker1 = new MarkerWithLabel({
+    //    position: homeLatLng,
+    //    draggable: true,
+    //    raiseOnDrag: true,
+    //    map: map,
+    //    labelContent: "$425K",
+    //    labelAnchor: new google.maps.Point(22, 0),
+    //    labelClass: "labels", // the CSS class for the label
+    //    labelStyle: {opacity: 0.75}
+    //  });
+		//
+		//
+		// var iw1 = new google.maps.InfoWindow({
+    //    content: $scope.restaurant.location.display_address[0] + "\n" + $scope.restaurant.location.display_address[1]
+    //  });
+    //  google.maps.event.addListener(marker1, "click", function (e) { iw1.open($scope.map, this); });
 
 });
