58a5a2d13fafb9a2377c074a462aa0a6c6dc2829
Irene Li <li.irene1@gmail.com>
2016-05-01 14:24:41 -0700

    commented out lots of stuff

--- seefood/www/module.js
+++ seefood/www/module.js
@@ -4,54 +4,59 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('seeFoodApp', ['ionic', 'ui.router', 'ngCordova', 'hmTouchEvents', 'angular-cache', 'firebase', 'uiGmapgoogle-maps'])
+angular.module('seeFoodApp', ['ionic'])
+// angular.module('seeFoodApp', ['ionic', 'ui.router', 'ngCordova', 'hmTouchEvents', 'angular-cache', 'firebase', 'uiGmapgoogle-maps', 'homeCtrl'])
 .constant('FirebaseUrl', 'http://seefoodapp.firebaseapp.com')
-.service('rootRef', ['FirebaseUrl', Firebase])
+// .service('rootRef', ['FirebaseUrl', Firebase])
 
-.config(function($stateProvider, $urlRouterProvider, CacheFactoryProvider) {
-  angular.extend(CacheFactoryProvider.defaults, { maxAge: 15 * 60 * 1000 });
+.config(function($stateProvider, $urlRouterProvider) {
+// .config(function($stateProvider, $urlRouterProvider, CacheFactoryProvider) {
+  // angular.extend(CacheFactoryProvider.defaults, { maxAge: 15 * 60 * 1000 });
 
+  console.log("config being hit");
   $stateProvider
   .state('home', {
     url: '/',
-    templateUrl: './home/partials/home.html'
-    //controller: 'homeCtrl'
+    templateUrl: 'home/partials/home.html',
+    controller: 'homeCtrl'
   })
-  .state('swipe', {
-    url: '/photos',
-    templateUrl: './swipe/partials/swipe.html',
-    controller: 'swipeCtrl',
-    onEnter: stateProtection
-  })
-  .state('list', {
-    url: '/list',
-    templateUrl: './list/partials/list.html',
-    controller: 'listCtrl',
-    onEnter: stateProtection
-  })
-  .state('detail', {
-    url: '/detail/:id',
-    templateUrl: './detail/partials/detail.html',
-    controller: 'detailCtrl',
-    onEnter: stateProtection
-  })
-  .state('login', {
-    url: '/login',
-    templateUrl: './templates/login.html',
-    controller: 'LoginCtrl as ctrl'
-  })
-  .state('email', {
-    url: '/email',
-    templateUrl: './email/partials/email.html',
-    controller: 'emailCtrl'
-  })
+  // .state('swipe', {
+  //   url: '/photos',
+  //   templateUrl: './swipe/partials/swipe.html',
+  //   controller: 'swipeCtrl',
+  //   onEnter: stateProtection
+  // })
+  // .state('list', {
+  //   url: '/list',
+  //   templateUrl: './list/partials/list.html',
+  //   controller: 'listCtrl',
+  //   onEnter: stateProtection
+  // })
+  // .state('detail', {
+  //   url: '/detail/:id',
+  //   templateUrl: './detail/partials/detail.html',
+  //   controller: 'detailCtrl',
+  //   onEnter: stateProtection
+  // })
+  // .state('login', {
+  //   url: '/login',
+  //   templateUrl: './templates/login.html',
+  //   controller: 'LoginCtrl as ctrl'
+  // })
+  // .state('email', {
+  //   url: '/email',
+  //   templateUrl: './email/partials/email.html',
+  //   controller: 'emailCtrl'
+  // })
 
   $urlRouterProvider.otherwise('/');
 })
 
 .constant('API', 'http://seefoodapp.herokuapp.com')
 
-.run(function($ionicPlatform, $cordovaGeolocation, RestaurantService) {
+.run(function($ionicPlatform) {
+
+//.run(function($ionicPlatform, $cordovaGeolocation, RestaurantService) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -61,13 +66,13 @@ angular.module('seeFoodApp', ['ionic', 'ui.router', 'ngCordova', 'hmTouchEvents'
       StatusBar.styleDefault();
     }
 
-    var posOptions = {
-      enableHighAccuracy: true,
-      timeout: 20000,
-      maximumAge: 0
-    };
+    // var posOptions = {
+    //   enableHighAccuracy: true,
+    //   timeout: 20000,
+    //   maximumAge: 0
+    // };
 
-    RestaurantService.findMe();
+    // RestaurantService.findMe();
   });
 });
 
