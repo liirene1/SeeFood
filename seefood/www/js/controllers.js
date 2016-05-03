c5a262cb033cdd0d654099ceec419425960b4375
Apple <tiffine.koch@gmail.com>
2016-05-02 20:51:35 -0700

    adding css

--- seefood/www/js/controllers.js
+++ seefood/www/js/controllers.js
@@ -1,26 +1,26 @@
-'use strict';
-
-var app = angular.module('seeFoodApp');
-
-app.controller('LoginCtrl', function(LoginCtrl) {
-
-  function LoginCtrl(Auth, $state) {
-    this.loginWithGoogle = function loginWithGoogle() {
-      Auth.$authWithOAuthPopup('google')
-      .then(function(authData) {
-        $state.go('tab.dash');
-      });
-    };
-  }
-  LoginCtrl.$inject = ['Auth', '$state'];
-
-  var ref = new Firebase("http://seefoodapp.firebaseapp.com");
-  ref.authWithOAuthRedirect("facebook", function(error) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      // We'll never get here, as the page will redirect on success.
-      $state.go("swipe");
-    }
-  });
-});
+// 'use strict';
+//
+// var app = angular.module('seeFoodApp');
+//
+// app.controller('LoginCtrl', function(LoginCtrl) {
+//
+//   function LoginCtrl(Auth, $state) {
+//     this.loginWithGoogle = function loginWithGoogle() {
+//       Auth.$authWithOAuthPopup('google')
+//       .then(function(authData) {
+//         $state.go('tab.dash');
+//       });
+//     };
+//   }
+//   LoginCtrl.$inject = ['Auth', '$state'];
+//
+//   var ref = new Firebase("http://seefoodapp.firebaseapp.com");
+//   ref.authWithOAuthRedirect("facebook", function(error) {
+//     if (error) {
+//       console.log("Login Failed!", error);
+//     } else {
+//       // We'll never get here, as the page will redirect on success.
+//       $state.go("swipe");
+//     }
+//   });
+// });
