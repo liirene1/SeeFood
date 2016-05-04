59d26b6f6f72db4ebdc618ef349bc9c7f2e71add
Josh Houck <Josh123090@gmail.com>
2016-05-04 14:29:00 -0700

    goog map

--- seefood/www/js/bundle.js
+++ seefood/www/js/bundle.js
@@ -79,32 +79,34 @@ app.controller('detailCtrl', ["$scope", "$stateParams", "RestaurantService", "$s
     return Math.round(m * 0.000621371192);
   };
 
-  // $scope.map = { center: {
-  //  latitude: $scope.restaurant.location.coordinate.latitude,
-  //  longitude: $scope.restaurant.location.coordinate.longitude },
-  //  zoom: 12 };
-  //
+  $scope.map = {
+    center: {
+      latitude: $scope.restaurant.location.coordinate.latitude,
+      longitude: $scope.restaurant.location.coordinate.longitude
+    },
+    zoom: 12
+  };
+
   // $scope.map.options = {
-  //     draggable: false,
-  //     labelContent: "lat: " + $scope.restaurant.location.coordinate.latitude + ' ' + 'lon: ' +  $scope.restaurant.location.coordinate.longitude,
-  //     labelAnchor: "100 0",
-  //     labelClass: "marker-labels"
-  //   };
+  //   draggable: true,
+  //   labelContent: "lat: " + $scope.restaurant.location.coordinate.latitude + ' ' + 'lon: ' +  $scope.restaurant.location.coordinate.longitude,
+  //   labelAnchor: "100 0",
+  //   labelClass: "marker-labels"
+  // };
 
   // var homeLatLng = new google.maps.LatLng($scope.restaurant.location.coordinate.latitude, $scope.restaurant.location.coordinate.longitude);
-  //
+
   // var marker1 = new MarkerWithLabel({
-  //    position: homeLatLng,
-  //    draggable: true,
-  //    raiseOnDrag: true,
-  //    map: map,
-  //    labelContent: "$425K",
-  //    labelAnchor: new google.maps.Point(22, 0),
-  //    labelClass: "labels", // the CSS class for the label
-  //    labelStyle: {opacity: 0.75}
+  //   position: homeLatLng,
+  //   draggable: true,
+  //   raiseOnDrag: true,
+  //   map: map,
+  //   labelContent: "$425K",
+  //   labelAnchor: new google.maps.Point(22, 0),
+  //   labelClass: "labels", // the CSS class for the label
+  //   labelStyle: {opacity: 0.75}
   //  });
-  //
-  //
+
   // var iw1 = new google.maps.InfoWindow({
   //    content: $scope.restaurant.location.display_address[0] + "\n" + $scope.restaurant.location.display_address[1]
   //  });
